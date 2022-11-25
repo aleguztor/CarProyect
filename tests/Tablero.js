9beec77b2fb5ed0ae1b5a4969f0e1e1dd2727b96
aleguztor <kihacaoficial@gmail.com>
2022-11-25 16:19:11 +0100

    error arreglado de deploy

--- tests/Tablero.js
+++
@@ -1,4 +0,0 @@
-import getObjectDescription from ["../js/Tablero.js"];
-describe('Test unitarios de nuestro módulo "getObjectDescription"', () => {
-    // En el caso de querer reiniciar o volver a instanciar nuestro módulo usaremos los métodos “beforeEach”, “afterEach”, “beforeAll” o “afterAll” según nos convenga.
-});
